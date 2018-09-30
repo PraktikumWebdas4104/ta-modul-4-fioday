6ea081d50c1473170a8bb293494d70984af83e3c
Dayana Alfionisya <43520177+fioday@users.noreply.github.com>
2018-09-30 20:24:25 +0700

    Update formawal.php
    form login

--- formawal.php
+++ formawal.php
@@ -1,5 +1,36 @@
-<?php
+<?php 
+session_start();
+error_reporting(0);
+		$user = array(
+				"user" => "dayana",
+				"pass" => "dayana"
+			);
 
+if(isset($_POST['submit'])){
+	if($_POST['username']==$user['user']&&$_POST['password']==$user['pass']) {
 
+		$_SESSION["username"] = $_POST['username'];
+		echo "Anda Berhasil Log In, $_POST[username]!";
 
-?>
+	} else {
+		display_login_form();
+		echo '<p> Username Atau Password Salah</p>';
+	}
+}
+else {
+	display_login_form();
+}
+function display_login_form(){ ?>
+	<form action ="proses.php" method='POST'>
+		<h1><center> Form Log In </center></h1>
+		<center><label for="username"> Username : </label></center>
+		<center><input type="text" name="username" id="username"></center>
+		<br><br>
+		<center><label for="password"> Password : </label></center>
+		<center><input type="password" name="password" id="password"></center>
+		<br><br>
+		<center><input type="submit" name="submit" value="Log In"></center>
+	</form>
+<?php 
+}
+ ?>
